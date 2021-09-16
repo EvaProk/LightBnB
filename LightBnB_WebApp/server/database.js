b20514d7b3ff1c2270f57b193158442902163d2e
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-16 23:40:25 +0000

    Add getAllProperties function query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -2,15 +2,14 @@ const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
 const { Pool } = require('pg');
+
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
   host: 'localhost',
   database: 'lightbnb'
 });
-// await client.connect()
 
-/// Users
 
 /**
  * Get a single user from the database given their email.
@@ -26,18 +25,6 @@ const getUserWithEmail = function(email) {
     return result.rows[0];
   })
   .catch((err) => err.message);
-  // return Promise.resolve(user);
-
-  // let user;
-  // for (const userId in users) {
-  //   user = users[userId];
-  //   if (user.email.toLowerCase() === email.toLowerCase()) {
-  //     break;
-  //   } else {
-  //     user = null;
-  //   }
-  // }
-  // return Promise.resolve(user);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -69,12 +56,6 @@ const addUser =  function(user) {
     .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
     .then(result => result)
     .catch((err) => err.message);
-
-
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
 }
 exports.addUser = addUser;
 
@@ -98,7 +79,6 @@ const getAllReservations = function(guest_id, limit = 10) {
   .then((result) => result.rows)
   .catch((err) => err.message);
 
-  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
@@ -111,16 +91,65 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool
-  .query(`SELECT * FROM properties LIMIT $1`, [limit])
-  .then((result) => result.rows)
-  .catch((err) => err.message);
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
 
-  // const limitedProperties = {};
-  // for (let i = 1; i <= limit; i++) {
-  //   limitedProperties[i] = properties[i];
-  // }
-  // return Promise.resolve(limitedProperties);
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id ) {
+    if(queryParams.length === 0){
+    queryParams.push(Number(options.owner_id));
+    queryString += `WHERE owner_id = $${queryParams.length} `
+    }else{
+      queryParams.push(`${options.owner_id}`);
+    queryString += `AND owner_id = $${queryParams.length} `;
+    } 
+  }
+
+  if (options.minimum_price_per_night) {
+    if(queryParams.length === 0){
+    queryParams.push(Number(options.minimum_price_per_night) * 100);
+    queryString += `WHERE cost_per_night > $${queryParams.length} `
+    }else{
+      queryParams.push(Number(options.minimum_price_per_night) * 100);
+    queryString += `AND cost_per_night > $${queryParams.length} `;
+    } 
+  }
+  if (options.maximum_price_per_night) {
+    if(queryParams.length === 0){
+    queryParams.push(Number(options.maximum_price_per_night) * 100);
+    queryString += `WHERE cost_per_night < $${queryParams.length} `
+    }else{
+      queryParams.push(Number(options.maximum_price_per_night) * 100);
+    queryString += `AND cost_per_night < $${queryParams.length} `;
+    } 
+  }
+
+  queryString += `GROUP BY properties.id `;
+
+  if (options.minimum_rating) {  
+    queryParams.push(Number(options.minimum_rating)); 
+    queryString += `HAVING AVG(rating) >= $${queryParams.length}`;
+  }
+  // 4
+  queryParams.push(limit);
+  queryString += `
+ 
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  // console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 
 }
 exports.getAllProperties = getAllProperties;
